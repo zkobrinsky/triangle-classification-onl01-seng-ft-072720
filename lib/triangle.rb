6367e9cd6e3dec6202c6450a23903317ea87c729
Zach Kobrinsky <zachkobrinsky@gmail.com>
2020-08-13 13:10:37 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -22,13 +22,10 @@ class Triangle
   end
 
   def valid?
-    # if
     if (@a <= 0 || @b <= 0 || @c <= 0)
       raise TriangleError
     elsif(@a + @b <= @c || @b + @c <= @a || @a + @c <= @b)
       raise TriangleError
-    # else
-    #   true
     end
   end
 
