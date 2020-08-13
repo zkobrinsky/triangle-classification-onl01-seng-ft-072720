64e69e436c06db59ce123a15ab92534f00e549da
Zach Kobrinsky <zachkobrinsky@gmail.com>
2020-08-13 12:51:12 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,38 @@
+require "pry"
+
 class Triangle
-  # write code here
+
+  attr_accessor :side1, :side2, :side3
+
+  def initialize(side1, side2, side3)
+    @side1 = side1
+    @side2 = side2
+    @side3 = side3
+  end
+
+  def kind
+    valid?
+
+    if (@side1 == @side2 && @side2 == @side3)
+      :equilateral
+    elsif (@side1 == @side2 || @side2 == @side3 || @side3 == @side1)
+      :isosceles
+    else
+      :scalene
+    end
+  end
+
+  # def valid?
+  #   if
+  #   if (@side1 <= 0 || @side2 <= 0 || @side3 <= 0)
+  #     raise TriangleError
+  #   end
+  # end
+
+
+  class TriangleError < StandardError
+  # triangle error code
+  end
+
+
 end
